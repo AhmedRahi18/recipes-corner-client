c5968be89aae0814e08b5f73537fd31899d82073
AhmedRahi18 <71ahmedrahi@gmail.com>
2023-05-04 05:46:16 +0600

    loader added

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,10 +1,18 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../AuthProvider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
+import { ClipLoader } from 'react-spinners';
 
 const PrivateRoute = ({children}) => {
-    const {user} = useContext(AuthContext)
+    const {user,loading} = useContext(AuthContext)
     const location = useLocation()
+    if(loading){
+        return <div className='text-center mt-52'>
+        <ClipLoader
+        loading={loading}
+        size={50}/>
+        </div>
+    }
     if(user){
         return children
     }
